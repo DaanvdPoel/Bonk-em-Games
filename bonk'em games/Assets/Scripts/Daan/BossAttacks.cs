c27f873905f87fd6fddd7624029ea7c72e5fd215
DaanvdPoel <200554@student.glu.nl>
2022-12-14 15:52:11 +0100

    .

--- bonk'em games/Assets/Scripts/Daan/BossAttacks.cs
+++ bonk'em games/Assets/Scripts/Daan/BossAttacks.cs
@@ -10,16 +10,31 @@ public class BossAttacks : MonoBehaviour
     [SerializeField] private float attackCooldown;
     [SerializeField] private Vector2 spreadField;
     private float time;
-     
+
     private void Update()
     {
         time = time + Time.deltaTime;
-        if(time >= attackCooldown)
+        NormalAttack();
+    }
+
+    private void NormalAttack()
+    {
+        if (time >= attackCooldown)
         {
             GameObject temp = Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, transform.position.z) + (new Vector3(transform.forward.x, 0, transform.forward.z) * 1.1f), Quaternion.identity);
             temp.transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x * Random.Range(-spreadField.x, spreadField.x), transform.rotation.y * Random.Range(-spreadField.y, spreadField.y), transform.rotation.z));
             temp.GetComponent<Rigidbody>().AddForce((new Vector3(transform.forward.x, bulletArc, transform.forward.z)) * bulletSpeed);
             time = 0;
         }
+    }
+
+    private void PushAttack()
+    {
+
+    }
+
+    private void BigAttack()
+    {
+
     }
 }
